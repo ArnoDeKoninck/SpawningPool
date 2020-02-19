ba637571d5094c4020231b0e3a8bcbbe414e3a9e
Arno De Koninck <arnodekoninck.contact@gmail.com>
2020-02-19 10:19:49 +0100

    v02192020- Barebones Colorpicker+Dark Theme

---
+++ Js/main.js
@@ -0,0 +1,66 @@
+//Opens and closes the SideMenu
+function menuToggle() {
+  let menu = document.getElementById("navMenu");
+  let container = document.getElementById("menuContainer");
+  let hero = document.getElementById("heroId");
+  menu.classList.toggle("menuToggle");
+  container.classList.toggle("open");
+  hero.classList.toggle("open");
+  //console.log(hero);
+}
+
+//Ico.ColorPicker API
+let colorPicker = new iro.ColorPicker("#dtColorPicker", {
+  width: 100,
+  color: "#f00"
+});
+let select = "";
+let dtHex = "";
+
+//Stores the currently selected color in dtHex
+function dtColorChange(color, changes) {
+  dtHex = colorPicker.color.hexString;
+  // print the color's new hex value to the developer console
+  //console.log(color.hexString);
+}
+
+// listen to a color picker's color:change event
+colorPicker.on("color:change", dtColorChange);
+function dtBgc() {
+  select = document.getElementById("navMenu");
+  select.style.backgroundColor = dtHex;
+}
+//Puts dtHex in all the selected items's css Color attribute
+function dtTxtc() {
+  select = document.getElementsByClassName("colorText");
+  for (let i = 0; i < select.length; i++) {
+    select[i].style.color = dtHex;
+  }
+}
+//DarkMode Switch
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+//Changes the page theme to Dark or Light
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
+  }
+}
+//Attempts to load any user preferences, else it write's one
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme, false);
